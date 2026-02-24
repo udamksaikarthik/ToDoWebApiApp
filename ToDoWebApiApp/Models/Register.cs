3b3605285c0774d92289d7294a8475d80c1bf6ca
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-24 10:55:25 +0000

    Authentication Model classes added

---
+++ ToDoWebApiApp/Models/Register.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoWebApiApp.Models
+{
+    public class Register
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
